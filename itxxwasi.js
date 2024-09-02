7f25f8eecbe82b320cccc28a1f10d36cbae86c64
OlaTheDev <olamilekandamilara83@gmail.com>
2024-09-02 06:01:27 +0100

    Update itxxwasi.js

--- itxxwasi.js
+++ itxxwasi.js
@@ -1,30 +1,41 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
+__path = process.cwd();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
+
 let server = require('./wasiqr'),
     code = require('./pair');
+
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/wasiqr', server);
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/wasipage.html')
-})
+
+// Apply body-parser middleware before routes
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.use('/wasiqr', server);
+app.use('/code', code);
+
+app.use('/pair', async (req, res, next) => {
+    res.sendFile(__path + '/pair.html');
+});
+
+app.use('/', async (req, res, next) => {
+    res.sendFile(__path + '/wasipage.html');
+});
+
+// Start the server
 app.listen(PORT, () => {
     console.log(`
 Don't Forget To Give Star
 
- Server running on http://localhost:` + PORT)
-})
+Server running on http://localhost:` + PORT);
+});
 
-module.exports = app
+module.exports = app;
+
 /**
     powered by Blake Hunter team 
     join Whatsapp channel for more updates 
-    **/
+**/
