99a66716742bf9cb8ce6e59b1edb26f417b757a9
OlaTheDev <olamilekandamilara83@gmail.com>
2024-09-02 06:02:18 +0100

    Update id.js

--- id.js
+++ id.js
@@ -1,10 +1,11 @@
 function makeid(num = 4) {
   let result = "";
   let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var characters9 = characters.length;
-  for (var i = 0; i < num; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters9));
+  let charactersLength = characters.length;
+  for (let i = 0; i < num; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
 }
-module.exports = {makeid};
+
+module.exports = { makeid };
