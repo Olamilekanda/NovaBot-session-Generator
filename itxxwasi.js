ca7effc0e231d248d477a8cd71688b60c3783eac
OlaTheDev <olamilekandamilara83@gmail.com>
2024-09-03 17:11:28 +0100

    Update itxxwasi.js

--- itxxwasi.js
+++ itxxwasi.js
@@ -1,41 +1,14 @@
 const express = require('express');
 const app = express();
-__path = process.cwd();
-const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8000;
+const wasiqr = require('./wasiqr');
+const pair = require('./Pair');
 
-let server = require('./wasiqr'),
-    code = require('./pair');
+// Use your routes
+app.use('/wasiqr', wasiqr);
+app.use('/pair', pair);
 
-require('events').EventEmitter.defaultMaxListeners = 500;
-
-// Apply body-parser middleware before routes
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Routes
-app.use('/wasiqr', server);
-app.use('/code', code);
-
-app.use('/pair', async (req, res, next) => {
-    res.sendFile(__path + '/pair.html');
-});
-
-app.use('/', async (req, res, next) => {
-    res.sendFile(__path + '/wasipage.html');
-});
-
-// Start the server
+// Set your port or use environment variable
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`
-Don't Forget To Give Star
-
-Server running on http://localhost:` + PORT);
+    console.log(`Server running on port ${PORT}`);
 });
-
-module.exports = app;
-
-/**
-    powered by Blake Hunter team 
-    join Whatsapp channel for more updates 
-**/
